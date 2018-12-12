14f2ea486621fe6b8a88bee97f13ba96da4640d1
Dima.Lavrisiuk <dmitriyemigrant@gmail.com>
2018-12-13 01:40:15 +0300

    Add new section

--- src/js/handlerScrollingNavigation.js
+++ src/js/handlerScrollingNavigation.js
@@ -1,9 +1,9 @@
 export default function handlerScrollingNavigation() {
     $(window).scroll(function () {
         if ($(window).scrollTop() >= $('header').height() + $('nav').height()*2) {
-            $('nav').addClass('navigation-is-scroll');
+            $('nav').addClass('navigation-is-scroll shadow-sm');
         } else {
-            $('nav').removeClass('navigation-is-scroll');
+            $('nav').removeClass('navigation-is-scroll shadow-sm');
         }
     });
 }
